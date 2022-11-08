6501ca2a98dbcaa252aa5bd72e09e978c901e6b0
leopauld ndenko <lndenko18@gmail.com>
2022-11-09 00:11:01 +0400

    files updated

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 const menuToggle = document.querySelector('.menu-toggle');
 
-console.log(menuToggle);
-
+const navigation = document.querySelector(".menu-list");
+console.log(navigation)
+ 
+// console.log(navigation, mobileToggle);
+menuToggle.addEventListener("click", () => {
+  const visibility = navigation.getAttribute("data-visible");
+   console.log(visibility);
+  if (visibility === "false") {
+    navigation.setAttribute("data-visible", true);
+    menuToggle.setAttribute("aria-expanded", true);
+  } else if (visibility === "true") {
+    navigation.setAttribute("data-visible", false);
+    menuToggle.setAttribute("aria-expanded", false);
+  }
+});
